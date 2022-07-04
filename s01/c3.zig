14f5724a5b7bc4ba46d8524c386f389fa191c8cf
Justin Davis <jrcd83@gmail.com>
2022-07-04 12:09:28 -0400

    S1C3 letter frequency routines
    Compares the calculated letter frequency to English letter frequency
    listed at Wikipedia: https://en.wikipedia.org/wiki/Letter_frequency

--- s01/c3.zig
+++ s01/c3.zig
@@ -2,27 +2,57 @@ const std = @import("std");
 const assert = std.debug.assert;
 const print = std.debug.print;
 const base64 = std.base64;
+const math = std.math;
 const mem = std.mem;
 const fmt = std.fmt;
 
 const cypher_hex = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
 const vowels = "aeiouyAEIOUY";
 
-fn score(english: []u8) f32 {
-    var result: f32 = 0.0;
-    for (english) |ch| {
-        if (ch < ' ' or ch > '~') return 0.0;
-        for (vowels) |v| {
-            if (v == ch) {
-                result += 1.0;
-                break;
-            }
-        }
+// const letter_range_count = '~' - ' ' + 1;
+const alpha_count = 'Z'-'A' + 1;
+var letter_freq: [alpha_count]f32 = undefined;
+var alpha_freq: [alpha_count]f32 = .{8.2, 1.5, 2.8, 4.3, 13.0, 2.2, 2.0, 6.1,
+                                     7.0, 0.15, 0.77, 4.0, 2.4, 6.7, 7.5, 1.9,
+                                     0.095, 6.0, 6.3, 9.1, 2.8, 0.98, 2.4,
+                                     0.15, 2.0, 0.074};
+
+fn letterFrequency(dest: []f32, text: []const u8) u64 {
+    var count: [alpha_count]u8 = undefined;
+    var n_alpha: u64 = 0;
+    assert(dest.len == alpha_count);
+
+    for (count) |_,i| {
+        count[i] = 0;
+        dest[i] = 0.0;
     }
-    return result;
+    for (text) |ch| {
+        var uch = ch & ~@as(u8, 0x20);
+        if (uch < 'A' or uch > 'Z') continue;
+        count[uch - 'A'] += 1;
+        n_alpha += 1;
+    }
+    for (count) |n,i| {
+        dest[i] = @intToFloat(f32, n) / @intToFloat(f32, n_alpha);
+    }
+
+    return n_alpha;
 }
 
-fn otp(dest: []u8, key: []const u8, ctext: []const u8) void {
+fn score(text: []u8) f32 {
+    for (text) |ch| {
+        if (ch < ' ' or ch > '~') return 0.0;
+    }
+    const n = letterFrequency(&letter_freq, text);
+    var sd: f32 = 0.0;
+    for (letter_freq) |p,i| {
+        sd += math.pow(f32, p - (alpha_freq[i] / 100.0), 2);
+    }
+    return (@intToFloat(f32, n) / @intToFloat(f32, text.len)) * (1.0 / sd);
+}
+
+// XOR one-time-pad
+fn xorOTP(dest: []u8, key: []const u8, ctext: []const u8) void {
     assert(ctext.len % key.len == 0);
     assert(dest.len == ctext.len);
     var i: u64 = 0;
@@ -32,6 +62,16 @@ fn otp(dest: []u8, key: []const u8, ctext: []const u8) void {
             dest[i + j] = ctext[i + j] ^ key[j];
         }
     }
+}
+
+test "trace" {
+    const msg = "Hello, world!".*;
+    const n = letterFrequency(&letter_freq, &msg);
+    print("DBG: {} total letters\n", .{n});
+    for (letter_freq) |p, i| {
+        print("DBG: {c} {}\n", .{@truncate(u8, 'A' + i), p});
+    }
+    print("\n", .{});
 }
 
 pub fn main() !void {
@@ -45,11 +85,11 @@ pub fn main() !void {
     var found: [1 + buf2.len]u8 = undefined;
     while (i > 0) : (i -= 1) {
         key[0] = i;
-        otp(&buf2, &key, cypher);
+        xorOTP(&buf2, &key, cypher);
         const x = score(&buf2);
         if (x == 0.0) continue;
-        print("{d}\t{s}\n", .{ i, &buf2 });
         if (x > max) {
+            print("{d}\t{}\t{s}\n", .{ i, x, &buf2 });
             max = x;
             found[0] = i;
             for (buf2) |ch, j| found[j + 1] = ch;
