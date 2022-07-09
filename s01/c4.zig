d7c7e4d5ade0ba26554a2dfaf9386ff4a632912e
Justin Davis <jrcd83@gmail.com>
2022-07-09 14:00:49 -0400

    Complete Challenge 4 Set 1

--- s01/c4.zig
+++ s01/c4.zig
@@ -8,7 +8,7 @@ const fmt = std.fmt;
 const sort = std.sort.sort;
 const fs = std.fs;
 
-const alloc_limit = 5 * 1024 * 1024;
+const alloc_limit = 128 * 1024;
 var alloc_buffer: [alloc_limit]u8 = .{0} ** alloc_limit;
 
 const alpha_count = 'Z' - 'A' + 1;
@@ -18,7 +18,12 @@ var alpha_freq: [alpha_count]f32 = .{ 8.2, 1.5, 2.8, 4.3, 13.0, 2.2, 2.0, 6.1, 7
 const printable_min = ' ';
 const printable_max = '~';
 
-fn slurpLines(path: [] const u8, allocator: std.mem.Allocator, limit: usize) ![][] const u8 {
+const LinesBuf = struct {
+    lines: [][] const u8,
+    buffer: []u8
+};
+
+fn slurpLines(path: [] const u8, allocator: std.mem.Allocator, limit: usize) !LinesBuf {
     var buf = try std.fs.cwd().readFileAlloc(allocator, path, limit);
     var iter = std.mem.split(u8, buf, "\n");
     var i: u64 = 0;
@@ -28,7 +33,10 @@ fn slurpLines(path: [] const u8, allocator: std.mem.Allocator, limit: usize) ![]
         lines[i] = line;
         i += 1;
     }
-    return lines;
+    return LinesBuf {
+        .lines = lines,
+        .buffer = buf
+    };
 }
 
 fn letterFrequency(dest: []f32, text: []const u8) u64 {
@@ -55,6 +63,7 @@ fn letterFrequency(dest: []f32, text: []const u8) u64 {
 
 fn score(text: []u8) f32 {
     for (text) |ch| {
+        if (ch == '\r' or ch == '\n' or ch == '\t') continue;
         if (ch < printable_min or ch > printable_max) return 0.0;
     }
 
@@ -65,18 +74,17 @@ fn score(text: []u8) f32 {
     }
 
     const alpha_ratio = @intToFloat(f32, n);
-    return (100.0 * alpha_ratio / @intToFloat(f32, text.len)) * (1.0 / math.sqrt(variance));
+    return (100.0 * alpha_ratio / @intToFloat(f32, text.len)) * (100.0 / math.sqrt(variance));
 }
 
 // XOR one-time-pad
 fn xorOTP(dest: []u8, key: []const u8, ctext: []const u8) void {
     assert(ctext.len % key.len == 0);
-    assert(dest.len >= ctext.len);
+    assert(dest.len == ctext.len);
     var i: u64 = 0;
     while (i < ctext.len) : (i += key.len) {
-        var j: u64 = 0;
-        while (j < key.len) : (j += 1) {
-            dest[i + j] = ctext[i + j] ^ key[j];
+        for (key) |k, j| {
+            dest[i + j] = ctext[i + j] ^ k;
         }
     }
 }
@@ -105,34 +113,32 @@ fn byScore(context: void, a: ScoreText, b: ScoreText) bool {
 const top_results_count = 5;
 
 fn crack(result: *ScoreText, cypher_hex: [] const u8, allocator: std.mem.Allocator) !bool {
-    var buf = try allocator.alloc(u8, cypher_hex.len / 2);
+    const text_len = cypher_hex.len / 2;
+    var buf = try allocator.alloc(u8, 256 * text_len);
     defer allocator.free(buf);
-    const cypher = try fmt.hexToBytes(buf, cypher_hex);
+
+    const cypher = try fmt.hexToBytes(buf[0..text_len], cypher_hex);
     var scored: [255]ScoreText = undefined;
 
-    for (scored) |*score_text| {
-        score_text.text = try allocator.alloc(u8, cypher.len);
-    }
-    defer {
-        for (scored) |score_text| {
-            allocator.free(score_text.text);
-        }
-    }
-    var i: u8 = 255;
+    // XOR decrypt with all possible one-byte keys (except for 0; that would be silly).
+    // Score each resulting plaintext.
+    var i: u64 = 255;
     while (i > 0) : (i -= 1) {
         var iter = &scored[i-1];
-        iter.key[0] = i;
-        xorOTP(iter.text, &iter.key, cypher);
-        iter.score = score(iter.text[0..]);
+        var text_dest = buf[i*text_len .. (i+1)*text_len];
+        iter.key[0] = @truncate(u8, i);
+        xorOTP(text_dest, &iter.key, cypher);
+        iter.text = text_dest;
+        iter.score = score(text_dest);
     }
 
     // print the top results
     sort(ScoreText, &scored, {}, byScore);
-    if (scored[0].score == 0.0) return false;
+    if (scored[0].score == 0.0) return false; // all scores are awful
     for (scored) |iter, j| {
         if (j > top_results_count) break;
         if (iter.score == 0.0) break;
-        print("{d}\t{}\t{d:1.2}\t{s}\n", .{ j+1, fmt.fmtSliceHexUpper(&iter.key), iter.score, iter.text });
+        print("*DBG* {d}\t{}\t{d:1.2}\t{s}\n", .{ j+1, fmt.fmtSliceHexUpper(&iter.key), iter.score, iter.text });
     }
 
     result.key = scored[0].key;
@@ -143,17 +149,29 @@ fn crack(result: *ScoreText, cypher_hex: [] const u8, allocator: std.mem.Allocat
 
 pub fn main() !void {
     var fba = std.heap.FixedBufferAllocator.init(&alloc_buffer);
+    defer fba.reset();
     var allocator = fba.allocator();
-    var hex_lines = try slurpLines("4.txt", allocator, alloc_buffer.len);
+    var lines_buf = try slurpLines("4.txt", allocator, alloc_buffer.len);
+    defer allocator.free(lines_buf.buffer);
+    const hex_lines = lines_buf.lines;
+
     print("*DBG* read {d} lines\n", .{hex_lines.len});
 
-    var buf: [128]u8 = .{0} ** 128;
-    var result: ScoreText = undefined;
-    result.text = &buf;
+    var buf1: [128]u8 = .{0} ** 128;
+    var buf2: [buf1.len]u8 = .{0} ** buf1.len;
+    var max = ScoreText { .score = 0.0, .key = .{0}, .text = &buf1 };
+    var max_i: u64 = 0;
+    var result = ScoreText { .score = 0.0, .key = .{0}, .text = &buf2 };
+
     for (hex_lines) |hex, i| {
-        print("*DBG* (i, {})\n", .{i});
         if (try crack(&result, hex, allocator)) {
-            print("{}\t{d:1.2}\t{s}\n", .{fmt.fmtSliceHexUpper(&result.key), result.score, result.text});
+            if (result.score > max.score) {
+                max.score = result.score;
+                max.key = result.key;
+                max_i = i;
+                std.mem.copy(u8, max.text, result.text);
+            }
         }
     }
+    print("(Line {d})\t(Key 0x{})\t(Score {d:1.2})\t{s}\n", .{max_i, fmt.fmtSliceHexUpper(&max.key), max.score, max.text});
 }
